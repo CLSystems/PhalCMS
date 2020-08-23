7b7e47338739b73a0057703ea8b37f5a7aab5dbb
Jeroen Guyt <2074126+orbex@users.noreply.github.com>
2020-08-23 08:04:19 +0200

    PCMS-3 Restore tests

--- src/Tasks/MainTask.php
+++ src/Tasks/MainTask.php
@@ -7,30 +7,30 @@ use Phalcon\Cli\Task;
 
 class MainTask extends Task
 {
-	/**
-	 * Main action for this task
-	 */
-	public function mainAction()
-	{
-		echo 'This is the default task and the default action' . PHP_EOL;
-	}
+    /**
+     * Main action for this task
+     */
+    public function mainAction()
+    {
+        echo 'This is the default task and the default action' . PHP_EOL;
+    }
 
-	/**
-	 * @param $output
-	 * @param Exception $e
-	 * @param Exception|null $previous
-	 */
-	protected function renderError($output, Exception $e, Exception $previous = null)
-	{
-		echo $output . PHP_EOL;
-		echo $e->getMessage() . PHP_EOL;
-		echo PHP_EOL;
-		echo $e->getTraceAsString();
-		if (null !== $previous) {
-			echo '====== previous ======' . PHP_EOL;
-			echo $previous->getMessage() . PHP_EOL;
-			echo PHP_EOL;
-			echo $previous->getTraceAsString();
-		}
-	}
+    /**
+     * @param $output
+     * @param Exception $e
+     * @param Exception|null $previous
+     */
+    protected function renderError($output, Exception $e, Exception $previous = null)
+    {
+        echo $output . PHP_EOL;
+        echo $e->getMessage() . PHP_EOL;
+        echo PHP_EOL;
+        echo $e->getTraceAsString();
+        if (null !== $previous) {
+            echo '====== previous ======' . PHP_EOL;
+            echo $previous->getMessage() . PHP_EOL;
+            echo PHP_EOL;
+            echo $previous->getTraceAsString();
+        }
+    }
 }
